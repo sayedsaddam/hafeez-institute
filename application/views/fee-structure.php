0e1c74fa2d5bd83cf255a6573ddffe31c62fb46f
Sayed Saddam Hussain <48250591+sayedsaddam@users.noreply.github.com>
2020-11-27 23:37:33 +0500

    Added images on the right side of the fee structure page.

--- application/views/fee-structure.php
+++ application/views/fee-structure.php
@@ -9,110 +9,121 @@
 </div>
 <div class="container">
     <div class="row mt-4">
-        <table class="table table-hover">
-            <tbody>
-                <tr>
-                    <td align="center" class="font-weight-bold" colspan="3">
-                        Note: Admission Fee once paid will not be refundable in any <br>
-                        Any query regarding fees will be entertained by Admission Committee.
-                    </td>
-                </tr>
-                <tr>
-                    <th>Course Name</th>
-                    <th>Admission Fee</th>
-                    <th>Semester Fee</th>
-                </tr>
-                <tr>
-                    <td class="font-weight-bold" align="center" colspan="3">Bachelor Program Fee</td>
-                </tr>
-                <tr>
-                    <td>B.Sc (Hons) Medical Laboratory Technology (Pathology)</td>
-                    <td>25,000/-</td>
-                    <td>40,000/-</td>
-                </tr>
-                <tr>
-                    <td>B.Sc (Hons) Dental Technology</td>
-                    <td>25,000/-</td>
-                    <td>60,000/-</td>
-                </tr>
-                <tr>
-                    <td>Doctor of Physical Therapy (DPT)</td>
-                    <td>25,000/-</td>
-                    <td>60,000/-</td>
-                </tr>
-                <tr>
-                    <td class="font-weight-bold" align="center" colspan="3">F.Sc COURSES FEE</td>
-                </tr>
-                <tr>
-                    <td>Admission and Monthly Fee</td>
-                    <td>18,000</td>
-                    <td>4,000/month</td>
-                </tr>
-                <tr>
-                    <td class="font-weight-bold" align="center" colspan="3">Short COURSES FEE</td>
-                </tr>
-                <tr>
-                    <td>Admission and Monthly Fee</td>
-                    <td>18,000</td>
-                    <td>4,000/month</td>
-                </tr>
-                <tr>
-                    <td class="font-weight-bold" align="center" colspan="3">OTHER FEE (APPLY TO ALL PROGRAMS)</td>
-                </tr>
-                <tr>
-                    <td>Security Fee</td>
-                    <td>5000/-</td>
-                    <td>Refundable</td>
-                </tr>
-                <tr>
-                    <td>Computer Lab Charges</td>
-                    <td>1000/-</td>
-                    <td>Annually Paid</td>
-                </tr>
-                <tr>
-                    <td>Library Charges</td>
-                    <td>1000/-</td>
-                    <td>Annually Paid</td>
-                </tr>
-                <tr>
-                    <td>Sports Charges</td>
-                    <td>1000/-</td>
-                    <td>Annually Paid</td>
-                </tr>
-                <tr>
-                    <td>College ID Card Fee:</td>
-                    <td>200/-</td>
-                    <td>Once Paid</td>
-                </tr>
-                <tr>
-                    <td>Departmental Labs Fee</td>
-                    <td>1500/-</td>
-                    <td>Annually Paid</td>
-                </tr>
-                <tr>
-                    <td>Univeristy/Board/Faculty Retention Fee</td>
-                    <td>Varies Annaually/-</td>
-                    <td>Annually Paid</td>
-                </tr>
-                <tr>
-                    <td>Univeristy/Board/Faculty Registration Fee:</td>
-                    <td>Varies Annaually/-</td>
-                    <td>Once Paid</td>
-                </tr>
-                <tr>
-                    <td class="font-weight-bold" align="center" colspan="3">OTHER FEE (APPLY TO ALL PROGRAMS)</td>
-                </tr>
-                <tr>
-                    <td>Govt Naseerullah Khan Babar Memorial Hospital Kohat Road Peshawar</td>
-                    <td>2000/-</td>
-                    <td>Per Month</td>
-                </tr>
-                <tr>
-                    <td>nstitute of Kidney Diseased(IKD) Hayatabad Peshawar</td>
-                    <td>2500/-</td>
-                    <td>Per Month</td>
-                </tr>
-            </tbody>
-        </table>
+        <div class="col-xl-lg-9 col-md-9 col-sm-12 mt-4">
+            <h5 class="mb-4">The fee structure is listed below.</h5>
+            <table class="table table-hover">
+                <tbody>
+                    <tr>
+                        <td align="center" class="font-weight-bold" colspan="3">
+                            Note: Admission Fee once paid will not be refundable in any <br>
+                            Any query regarding fees will be entertained by Admission Committee.
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>Course Name</th>
+                        <th>Admission Fee</th>
+                        <th>Semester Fee</th>
+                    </tr>
+                    <tr>
+                        <td class="font-weight-bold" align="center" colspan="3">Bachelor Program Fee</td>
+                    </tr>
+                    <tr>
+                        <td>B.Sc (Hons) Medical Laboratory Technology (Pathology)</td>
+                        <td>25,000/-</td>
+                        <td>40,000/-</td>
+                    </tr>
+                    <tr>
+                        <td>B.Sc (Hons) Dental Technology</td>
+                        <td>25,000/-</td>
+                        <td>60,000/-</td>
+                    </tr>
+                    <tr>
+                        <td>Doctor of Physical Therapy (DPT)</td>
+                        <td>25,000/-</td>
+                        <td>60,000/-</td>
+                    </tr>
+                    <tr>
+                        <td class="font-weight-bold" align="center" colspan="3">F.Sc COURSES FEE</td>
+                    </tr>
+                    <tr>
+                        <td>Admission and Monthly Fee</td>
+                        <td>18,000</td>
+                        <td>4,000/month</td>
+                    </tr>
+                    <tr>
+                        <td class="font-weight-bold" align="center" colspan="3">Short COURSES FEE</td>
+                    </tr>
+                    <tr>
+                        <td>Admission and Monthly Fee</td>
+                        <td>18,000</td>
+                        <td>4,000/month</td>
+                    </tr>
+                    <tr>
+                        <td class="font-weight-bold" align="center" colspan="3">OTHER FEE (APPLY TO ALL PROGRAMS)</td>
+                    </tr>
+                    <tr>
+                        <td>Security Fee</td>
+                        <td>5000/-</td>
+                        <td>Refundable</td>
+                    </tr>
+                    <tr>
+                        <td>Computer Lab Charges</td>
+                        <td>1000/-</td>
+                        <td>Annually Paid</td>
+                    </tr>
+                    <tr>
+                        <td>Library Charges</td>
+                        <td>1000/-</td>
+                        <td>Annually Paid</td>
+                    </tr>
+                    <tr>
+                        <td>Sports Charges</td>
+                        <td>1000/-</td>
+                        <td>Annually Paid</td>
+                    </tr>
+                    <tr>
+                        <td>College ID Card Fee:</td>
+                        <td>200/-</td>
+                        <td>Once Paid</td>
+                    </tr>
+                    <tr>
+                        <td>Departmental Labs Fee</td>
+                        <td>1500/-</td>
+                        <td>Annually Paid</td>
+                    </tr>
+                    <tr>
+                        <td>Univeristy/Board/Faculty Retention Fee</td>
+                        <td>Varies Annaually/-</td>
+                        <td>Annually Paid</td>
+                    </tr>
+                    <tr>
+                        <td>Univeristy/Board/Faculty Registration Fee:</td>
+                        <td>Varies Annaually/-</td>
+                        <td>Once Paid</td>
+                    </tr>
+                    <tr>
+                        <td class="font-weight-bold" align="center" colspan="3">OTHER FEE (APPLY TO ALL PROGRAMS)</td>
+                    </tr>
+                    <tr>
+                        <td>Govt Naseerullah Khan Babar Memorial Hospital Kohat Road Peshawar</td>
+                        <td>2000/-</td>
+                        <td>Per Month</td>
+                    </tr>
+                    <tr>
+                        <td>Institute of Kidney Diseased(IKD) Hayatabad Peshawar</td>
+                        <td>2500/-</td>
+                        <td>Per Month</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="col-xl-lg-3 col-md-3 col-sm-12 mt-4 text-center">
+            <img src="<?= base_url('assets/img/hims-logo.png'); ?>" alt="hims-logo">
+            <img src="<?= base_url('assets/img/about-02.jpg'); ?>" class="img-fluid mt-4" alt="about-hims">
+            <img src="<?= base_url('assets/img/about-03.jpg'); ?>" class="img-fluid mt-4" alt="about-hims">
+            <img src="<?= base_url('assets/img/about-02.jpg'); ?>" class="img-fluid mt-4" alt="about-hims">
+            <img src="<?= base_url('assets/img/about-03.jpg'); ?>" class="img-fluid mt-4" alt="about-hims">
+            <img src="<?= base_url('assets/img/about-02.jpg'); ?>" class="img-fluid mt-4" alt="about-hims">
+        </div>
     </div>
 </div>
