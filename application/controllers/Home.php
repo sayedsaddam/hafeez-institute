a40d863d0611d90018d2617f8718d8e385e2265b
Sayed Saddam Hussain <48250591+sayedsaddam@users.noreply.github.com>
2020-11-27 21:47:37 +0500

    Created new page for about, modified 3 files including controller, views, and config file for route setting.

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -6,9 +6,16 @@ class Home extends CI_Controller{
     function __construct(){
         parent::__construct();
     }
+    // Home page will be loaded when this class is called.
     public function index(){
-        $data['title'] = 'Hafeez Institute of Medical Sciences| Home';
+        $data['title'] = 'Hafeez Institute of Medical Sciences | Home';
         $data['body'] = 'home';
+        $this->load->view('components/template', $data);
+    }
+    // About
+    public function about(){
+        $data['title'] = 'Hafeez Institute of Medical Sciences | About';
+        $data['body'] = 'about';
         $this->load->view('components/template', $data);
     }
 }
---
+++ application/views/about.php
@@ -0,0 +1,36 @@
+<div class="container">
+    <div class="row mt-4 pt-4">
+        <div class="col-xl-lg-12 col-md-12 col-sm-12 bg-info rounded">
+            <h1 class="display-4 font-weight-bold text-white">About Us | <small>We are Professional</small></h1>
+        </div>
+    </div>
+    <div class="row mt-4">
+        <div class="col-xl-lg-6 col-md-6 col-sm-12 bg-info rounded text-white pt-4">
+            <h5 class="font-weight-bold pt-4 mt-4">
+                Welcome to Hafeez Institute of Medical Sciences
+            </h5><hr style="background-color: white;">
+            <p class="text-justify">
+            Hafeez Institute of Medical Sciences is a poineer Institute of Khyber Pakhtunkhwa established in 2007. This institute is working under the Health Care Organization registered with Social Welfare Department Khyber Pakhtunkhwa. This is the first institute in Khyber Pakhtunkhwa that introduced F.Sc and Paramedical Program in the province. The idea was conceived by group of highly professional and dedicated healthcare professionals. The concept is to give standard medical education in Pakistan ensuring that students of this Institute must have the mindset of a true human professional. Hafeez Institute of Medical Sciences is affiliated with BISE Peshawar for F.Sc Medical Technologies, Khyber Pakhtunkhwa Medical Faculty (Health Department) for Diploma Courses, Khyber Medical University for Medical Laboratory Technology and Bacha Khan University for Master Programs and Paramedical Degree Programs and Doctor of Physical Therapy (DPT). The students of this institute availing practical/clinical training in different Government Hospital on the basis of the public private partnership.
+            </p>
+            <img src="<?= base_url('assets/img/about-02.jpg'); ?>" class="img-fluid rounded" alt="About-hims">
+        </div>
+        <div class="col-xl-lg-6 col-md-6 col-sm-12 pt-4">
+            <h5 class="font-weight-bold">Aim</h5><hr>
+            <p class="text-justify">Hafeez Institute of Medical Sciences is committed for one ultimate goal of offering community oriented Paramedical education to produce professional manpower and services at all level of health care-preventive, promotive, curative & rehabilitative by experienced, by experienced, efficient and expertise staff in various specialties so as to meet the primary requirement of “Health for All”.</p>
+            <h5 class="font-weight-bold">Vision</h5><hr>
+            <p class="text-justify">Hafeez Institute of Medical Sciences has a vision to provide highly professional and dedicated healthcare professionals in the province.</p>
+            <h5 class="font-weight-bold">Mission</h5><hr>
+            <p class="text-justify">The Mission of Hafeez Institute of Medical Sciences is to equip the students with relevant professional knowledge, skills, techniques, and ethical values to enable them to apply their acquired expertise at level between the doctors and the patient for efficient health service delivery.</p>
+            <h5 class="font-weight-bold">Objective</h5><hr>
+            <p class="text-justify">The Objectives of Hafeez Institute of Medical Sciences are as under :-</p>
+            <ul>
+                <li>Develop accuracy and meticulousness to attain high levels of ethics and technical proficiency.</li>
+                <li>Develop good relationship, problem solving and administrative skills.</li>
+                <li>Develop and analyze innovative strategies for effective communication with the patients and the health care personal.</li>
+                <li>Demonstrate understanding of the basic concepts of professional behavior and legal implications of the work environment.</li>
+                <li>Demonstrate the knowledge of his /her role in health care delivery system.</li>
+                <li>Establishing and maintaining continuing education as a function of growth and maintenance of professional competence.</li>
+            </ul>
+        </div>
+    </div>
+</div>
